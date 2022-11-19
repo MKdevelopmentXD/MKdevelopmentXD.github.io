04b89e8ec5e53bebcb9439ce163e5d4fcbfd520a
MKdevelopmentXD <102597723+MKdevelopmentXD@users.noreply.github.com>
2022-11-20 01:07:59 +0200

    Add files via upload

---
+++ LR_8_JS/src/Data.js
@@ -0,0 +1,59 @@
+let shopItemsData = [
+  {
+    id: "jfhgbvnscs",
+    name: "Prestigio SmartBook",
+    price: 9999,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-1.jpg",
+  },
+  {
+    id: "ioytrhndcv",
+    name: "CHUWI GemiBook",
+    price: 17990,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-2.jpg",
+  },
+  {
+    id: "wuefbncxbsn",
+    name: "YEPO 737N16",
+    price: 19990,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-3.jpg",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "Lenovo Yoga",
+    price: 38709,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-4.jpg",
+  },
+  {
+    id: "tsdayfhcbcv",
+    name: "Lenovo IdeaPad 3 15IGL05",
+    price: 14999,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-5.jpg",
+  },
+  {
+    id: "tertedsfsdv",
+    name: "HP Elite Dragonfly G2 Galaxy Blue",
+    price: 86020,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-6.jpg",
+  },
+  {
+    id: "asdasd",
+    name: "ASUS X513EA-BQ2805",
+    price: 30106,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-7.jpg",
+  },
+  {
+    id: "tedsfsdfbcv",
+    name: "CHUWI HeroBook Air (4/128)",
+    price: 10999,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-8.jpg",
+  },
+  
+];
